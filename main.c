fa9b0418bea658f99662c4a5c9c52fc8a3989a2f
Gustav Louw <airglouw@gmail.com>
2019-08-03 12:35:00 -0700

    cleaning up old projects

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 
 typedef struct
 {
-    int** grid;
+    int* grid;
     int size;
 }
 Map;
@@ -14,134 +14,126 @@ typedef struct
     int x;
     int y;
 }
-Point;
+Cart;
 
 typedef struct
 {
-    Point a;
-    Point b;
+    Cart a;
+    Cart b;
 }
 Mesh;
 
-static void bterp(const Map map, const Mesh m)
+static void Interpolate(Map* map, const Mesh m)
 {
     for(int x = m.a.x; x < m.b.x; x++)
     for(int y = m.a.y; y < m.b.y; y++)
-    map.grid[x][y] = (
-        map.grid[m.a.x][m.a.y] * (m.b.x - x) * (m.b.y - y) +
-        map.grid[m.b.x][m.a.y] * (x - m.a.x) * (m.b.y - y) +
-        map.grid[m.a.x][m.b.y] * (m.b.x - x) * (y - m.a.y) +
-        map.grid[m.b.x][m.b.y] * (x - m.a.x) * (y - m.a.y)
-    ) / ((m.b.x - m.a.x) * (m.b.y - m.a.y));
+        map->grid[y + map->size * x] = (
+            map->grid[m.a.y + map->size * m.a.x] * (m.b.x - x) * (m.b.y - y) +
+            map->grid[m.a.y + map->size * m.b.x] * (x - m.a.x) * (m.b.y - y) +
+            map->grid[m.b.y + map->size * m.a.x] * (m.b.x - x) * (y - m.a.y) +
+            map->grid[m.b.y + map->size * m.b.x] * (x - m.a.x) * (y - m.a.y)
+        ) / ((m.b.x - m.a.x) * (m.b.y - m.a.y));
 }
 
-static void noise(const Map map, const int square)
+static void Noise(Map* map, const int square)
 {
     if(square == 2)
         return;
     const int m = (square - 1) / 2;
-    /* Points of interest */
-    for(int i = m; i < map.size; i += 2 * m)
-    for(int j = m; j < map.size; j += 2 * m)
-        map.grid[i][j] += rand() % (2 * square + 1) - square;
-    /* Interpolation */
-    for(int i = 0; i < map.size - 1; i += m)
-    for(int j = 0; j < map.size - 1; j += m)
+    for(int i = m; i < map->size; i += 2 * m)
+    for(int j = m; j < map->size; j += 2 * m)
+        map->grid[j + map->size * i] += rand() % (2 * square + 1) - square;
+    for(int i = 0; i < map->size - 1; i += m)
+    for(int j = 0; j < map->size - 1; j += m)
     {
         const Mesh mesh = { { i, j }, { i + m, j + m } };
-        bterp(map, mesh);
+        Interpolate(map, mesh);
     }
-    /* Down the rabbit hole we go */
-    noise(map, m + 1);
+    Noise(map, m + 1);
 }
 
-static int max(const Map map)
+static int Max(Map* map)
 {
     int max = INT_MIN;
-    for(int i = 0; i < map.size; i++)
-    for(int j = 0; j < map.size; j++)
-        if(map.grid[i][j] > max)
-            max = map.grid[i][j];
+    for(int i = 0; i < map->size; i++)
+    for(int j = 0; j < map->size; j++)
+        if(map->grid[j + map->size * i] > max)
+            max = map->grid[j + map->size * i];
     return max;
 }
 
-static int min(const Map map)
+static int Min(Map* map)
 {
     int min = INT_MAX;
-    for(int i = 0; i < map.size; i++)
-    for(int j = 0; j < map.size; j++)
-        if(map.grid[i][j] < min)
-            min = map.grid[i][j];
+    for(int i = 0; i < map->size; i++)
+    for(int j = 0; j < map->size; j++)
+        if(map->grid[j + map->size * i] < min)
+            min = map->grid[j + map->size * i];
     return min;
 }
 
-static void add(const Map map, const int val)
+static void Add(Map* map, const int val)
 {
-    for(int i = 0; i < map.size; i++)
-    for(int j = 0; j < map.size; j++)
-        map.grid[i][j] += val;
+    for(int i = 0; i < map->size; i++)
+    for(int j = 0; j < map->size; j++)
+        map->grid[j + map->size * i] += val;
 }
 
-static void normalize(const Map map)
+static void Normalize(Map* map)
 {
-    const int lowest = min(map);
-    add(map, abs(lowest));
-    const int highest = max(map);
-    for(int i = 0; i < map.size; i++)
-    for(int j = 0; j < map.size; j++)
-        map.grid[i][j] = 0xFF * map.grid[i][j] / (float) highest;
+    const int lowest = Min(map);
+    Add(map, abs(lowest));
+    const int highest = Max(map);
+    for(int i = 0; i < map->size; i++)
+    for(int j = 0; j < map->size; j++)
+        map->grid[j + map->size * i] = 0xFF * map->grid[j + map->size * i] / (float) highest;
 }
 
-static void draw(const Map map, SDL_Renderer* const renderer)
+static void Draw(Map* map, SDL_Renderer* const renderer)
 {
-    normalize(map);
-    for(int i = 0; i < map.size; i++)
-    for(int j = 0; j < map.size; j++)
+    Normalize(map);
+    for(int i = 0; i < map->size; i++)
+    for(int j = 0; j < map->size; j++)
     {
-        const int a = 0x2A; /* Sea floor */
-        const int b = 0x5F; /* Sea level */
-        const int p = map.grid[i][j];
+        const int a = 0x2A; // Sea floor.
+        const int b = 0x5F; // Sea level.
+        const int p = map->grid[j + map->size * i];
         p < a ?
-            SDL_SetRenderDrawColor(renderer, 0, 0, a, 0): /* Sea floor */
+            SDL_SetRenderDrawColor(renderer, 0, 0, a, 0): // Sea floor.
         p < b ?
-            SDL_SetRenderDrawColor(renderer, 0, 0, p, 0): /* Sea */
-            SDL_SetRenderDrawColor(renderer, p, p, p, 0); /* Ice and snow */
+            SDL_SetRenderDrawColor(renderer, 0, 0, p, 0): // Sea.
+            SDL_SetRenderDrawColor(renderer, p, p, p, 0); // Ice and snow.
         SDL_RenderDrawPoint(renderer, i, j);
     }
     SDL_RenderPresent(renderer);
-    SDL_Delay(5e3);
+    SDL_Delay(1000);
 }
 
-static Map build(const int size)
+Map MP_Build(const int power)
 {
-    #define toss(type, size) ((type*) (calloc(size, sizeof(type))))
-    Map map;
-    map.grid = toss(int*, size);
-    for(int i = 0; i < size; i++)
-        map.grid[i] = toss(int, size);
+    const int size = pow(2, power) + 1;
+    Map map = { 0 };
+    map.grid = calloc(size * size, sizeof(*map.grid));
     map.size = size;
-    #undef toss
+    Noise(&map, map.size);
     return map;
 }
 
-static void clean(const Map map)
+void MP_Clean(Map* map)
 {
-    for(int i = 0; i < map.size; i++)
-        free(map.grid[i]);
-    free(map.grid);
+    free(map->grid);
 }
 
 int main()
 {
     srand(time(0));
-    const int res = pow(2, 9) + 1;
+    const int power = 9;
+    Map map = MP_Build(power);
     SDL_Window* window;
     SDL_Renderer* renderer;
-    SDL_CreateWindowAndRenderer(res, res, 0, &window, &renderer);
-    const Map map = build(res);
-    noise(map, map.size);
-    draw(map, renderer);
-    clean(map);
+    SDL_CreateWindowAndRenderer(map.size, map.size, 0, &window, &renderer);
+    Draw(&map, renderer);
+    MP_Clean(&map);
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     return 0;
